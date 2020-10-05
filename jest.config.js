9691dfe8da28197e7487afdf23b85a1b3060cf56
zarbielli <jlfz06@gmail.com>
2020-10-05 19:27:15 -0300

    Changing jest testEnvirioment to fix compatibility error

--- jest.config.js
+++ jest.config.js
@@ -133,7 +133,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -142,7 +142,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/src/tests/**/*.ts"],
+  testMatch: ['**/src/tests/**/*.ts']
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -184,4 +184,4 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-};
+}
