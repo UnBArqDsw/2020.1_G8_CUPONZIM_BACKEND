7b8e754c4957217cf4b68cc07661bf369eb269b7
André <andreabenf@gmail.com>
2020-10-18 23:23:33 -0300

    commiting tsconfig and changing variables to env

--- typeOrmConfig.ts
+++ typeOrmConfig.ts
@@ -4,9 +4,9 @@ const typeOrmConfig: PostgresConnectionOptions = {
   type: 'postgres',
   host: 'db',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'cuponzim',
+  username: String(process.env.DB_USERNAME),
+  password: String(process.env.DB_PASSWORD),
+  database: String(process.env.DB_DATABASE),
   synchronize: true,
   logging: false,
   entities: ['src/entities/**/*.ts', 'dist/entities/**/*.js']
