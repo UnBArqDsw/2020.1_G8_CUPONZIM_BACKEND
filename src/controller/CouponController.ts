077fa80f6345c681870b3ae7310a14051012c7ef
zarbielli <jlfz06@gmail.com>
2020-11-17 15:37:08 -0300

    Adjusting Coupon controller

--- src/controller/CouponController.ts
+++ src/controller/CouponController.ts
@@ -1,118 +1,76 @@
-import { getRepository } from 'typeorm';
-import { NextFunction, Request, Response } from 'express';
+import { getRepository } from 'typeorm'
+import { NextFunction, Request, Response } from 'express'
 import { Coupon } from '../entity/Coupon'
 import AuthController from './Authcontroller'
 
 export class CouponController {
   private CouponRepository = getRepository(Coupon)
 
-  verifyToken(request: Request) {
+  private verifyToken(request: Request) {
     const jwt = request.header['Authorization']
-    let auth = new AuthController();
-    const canUseRoute = auth.checkJwt(jwt);
-    if (canUseRoute) return true;
-    else return false;
+    const auth = new AuthController()
+    const canUseRoute = auth.checkJwt(jwt)
+    if (canUseRoute) return true
+    else return false
+  }
+
+  private tokenMiddleware(response: Response, hasToken: boolean, dbResponse) {
+    return hasToken ? dbResponse : response.status(401).send('Authorization failed')
+  }
+  
+  private async findLot(request: Request, response: Response) {
+    try {
+      const dbResponse = await this.CouponRepository.find({
+        where: [
+          { location_id: request.body.location_id },
+          { shop_id: request.body.shop_id }
+        ]
+      })
+      return dbResponse
+    } catch {
+      return response.status(404)
+    }
+  }
+
+  private async updateLot(request: Request, response: Response) {
+    try {
+      const lot = await this.CouponRepository.findOne({ where: { coupon_id: request.body.coupon_id } })
+      const coupon = await this.CouponRepository.save({
+        ...lot,
+        ...request.body
+      })
+      return coupon
+    } catch {
+      return response.status(404)
+    }
   }
 
   //  POST /lot
   async CreateLot(request: Request, response: Response, next: NextFunction): Promise<Coupon | void | JSON> {
-    const canUseRoute =  this.verifyToken(request)
-    if (canUseRoute) {
-      let params = {
-        lot: await this.CouponRepository.save(request.body),
-        status: 200
-      }
-       response.status(200).send(params)
-    } else {
-      let params = {
-        Error: "Authorization falied",
-        status: 401
-      }
-      response.status(401).send(params)
-    }
+    return this.tokenMiddleware(response, this.verifyToken(request), await this.CouponRepository.save(request.body))
   }
 
   // GET /lot
   async GetLot(request: Request, response: Response, next: NextFunction): Promise<Coupon | void> {
-    const canUseRoute =  this.verifyToken(request)
-    if (canUseRoute) {
-      let params = {
-        lot: await this.CouponRepository.find({
-          where: [
-            { location_id: request.body.location_id },
-            { shop_id: request.body.shop_id }
-          ]
-        }),
-        status: 200
-      }
-      response.status(200).send(params)
-    } else {
-      let params = {
-        Error: "Authorization falied",
-        status: 401
-      }
-      response.status(401).send(params)
-    }
+   return this.tokenMiddleware(response, this.verifyToken(request), await this.findLot(request, response))     
   }
 
   // PUT /lot
   async UpdateLot(request: Request, response: Response, next: NextFunction): Promise<Coupon | void> {
-    const canUseRoute = this.verifyToken(request)
-    const lot = await this.CouponRepository.findOne({ where: { lot_id: request.body.lot_id } })
-
-    if (canUseRoute) {
-      let params = {
-        lot: await this.CouponRepository.save({
-          ...lot,
-          ...request.body
-        }),
-        status: 200
-      }
-      response.status(200).send(params)
-    } else {
-      let params = {
-        Error: "Authorization falied",
-        status: 401
-      }
-      response.status(401).send(params)
-    }
+    return this.tokenMiddleware( response, 
+      this.verifyToken(request), 
+      await this.CouponRepository.find({ where: { lot_id: request.body.lot_id }, take: 1 }))
   }
 
   // DELETE /lot
   async DeleteLot(request: Request, response: Response, next: NextFunction): Promise<Coupon | void> {
-    const canUseRoute = this.verifyToken(request)
-    const lotToRemove = await this.CouponRepository.findOne({ where: { lot_id: request.body.lot_id } })
-    if (canUseRoute) {
-      let params = {
-        lot: this.CouponRepository.remove(lotToRemove),
-        status: 200
-      }
-      response.status(200).send(params)
-    } else  {
-      let params = {
-        Error: "Authorization falied",
-        status: 401
-      }
-      response.status(401).send(params)
-    }
+    return this.tokenMiddleware(response,
+      this.verifyToken(request),
+      await this.CouponRepository.find({ where: { lot_id: request.body.lot_id }, take:1 }))
   }
-
 
   // POST /coupon/SetCouponAsUsed
   async SetCouponAsUsed(request: Request, response: Response, next: NextFunction): Promise<Coupon | void> {
-    const canUseRoute = this.verifyToken(request)
-    
-    const lot = await this.CouponRepository.findOne({ where: { coupon_id: request.body.coupon_id } })
-    let responseBody = {
-      coupon: await this.CouponRepository.save({
-        ...lot,
-        ...request.body
-      }),
-      status: 200
-    }
-    if (lot) {
-      response.status(200).send(responseBody)
-    } else return; 
+    return this.tokenMiddleware(response, this.verifyToken(request),await this.updateLot(request, response))
   }
-
 }
