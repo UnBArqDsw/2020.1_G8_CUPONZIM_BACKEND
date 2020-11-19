82e3d1f3386552d0d3db7d3a97eedbab32ec900e
jonjon667 <jonjon667@hotmail.com>
2020-11-19 17:17:03 -0300

    Iniciating ShopOwner entity

--- src/controller/Authcontroller.ts
+++ src/controller/Authcontroller.ts
@@ -2,14 +2,16 @@
 import { getRepository } from 'typeorm'
 import { NextFunction, Request, Response } from 'express'
 import { Client } from '../entity/Client'
+import { ShopOwner } from '../entity/ShopOwner'
 import jwt from 'jsonwebtoken'
 import config from '../config/config'
 import md5 from 'md5'
+import { Shop } from 'entity/Shop'
 
 export default class AuthController {
   private ClientRepository = getRepository(Client);
-
-  async login (request: Request, response: Response, next: NextFunction): Promise<void> {
+  private OwnerRepository = getRepository(ShopOwner);
+  async login(request: Request, response: Response, next: NextFunction): Promise<void> {
     // Check if username and password are set
     const { username, password } = request.body
     const pass = md5(password + config)
@@ -18,21 +20,32 @@ export default class AuthController {
     }
 
     let user: Client
+    let shopOwner: ShopOwner
     try {
-      user = await this.ClientRepository.findOneOrFail({ where: { username_client: username, password_client: pass } })
-      const token = jwt.sign(
-        { userId: user.idClient, username: user.idClient },
-        <string>config,
-        { expiresIn: '1h' }
-      )
+      if (request.body.isShopOwner){
+        shopOwner = await this.OwnerRepository.findOneOrFail({ where: { username_shop: username, password_shop: pass } })
+        const token = jwt.sign(
+          { shopOwnerId: shopOwner.idShopOwner, username: shopOwner.username_shop },
+          <string>config,
+          { expiresIn: '10h' }
+        )
+      }
+      else {
+        user = await this.ClientRepository.findOneOrFail({ where: { username_client: username, password_client: pass } })
 
+        const token = jwt.sign(
+          { userId: user.idClient, username: user.idClient },
+          <string>config,
+          { expiresIn: '1h' }
+        )
+      }
       response.status(200).send({ Success: true, Token: token })
     } catch (error) {
       response.status(401).send({ Erro: 'Não foi possível encontrar usuário com as credenciais fornecidas ' })
     }
   }
 
-  async checkJwt (tokenToVerify: string): Promise<boolean> {
+  async checkJwt(tokenToVerify: string): Promise<boolean> {
     // Get the jwt token from the head
     const token = <string>tokenToVerify
 
---
+++ src/entity/ShopOwner.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, JoinTable } from 'typeorm'
+import { Shop } from './Shop'
+@Entity()
+export class ShopOwner {
+   @PrimaryGeneratedColumn()
+   idShopOwner: number;
+
+   @Column('varchar', { unique: true, length: 50 })
+   username_shop: string;
+
+   @Column('varchar', { length: 50 })
+   password_shop: string;
+
+   @OneToMany(type => Shop,shop => shop.shopOwner,{ eager: true })
+   @JoinTable()
+   shops: Shop[]
+}
