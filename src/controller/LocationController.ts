e9f238592167df7c779ee18008688e7719a6b010
zarbielli <jlfz06@gmail.com>
2020-11-19 03:44:20 -0300

    Adjusting Location controller to the new TokenVerifier middleware logic
    Co-authored-by: jonjon667 <jonjon667@gmail.com>

--- src/controller/LocationController.ts
+++ src/controller/LocationController.ts
@@ -1,42 +1,31 @@
 import { getRepository, Any } from 'typeorm';
 import { NextFunction, Request, Response } from 'express';
 import { Location } from '../entity/Location'
-import AuthController from './Authcontroller'
+import  TokenVerifier from '../Middleware/TokenVerifier'
+
 export class LocationController {
-    private LocationRepository = getRepository(Location);
+  private LocationRepository = getRepository(Location);
+  private TokenVerifier = new TokenVerifier()
 
-    private verifyToken(request: Request) {
-        const jwt = request.headers.authorization
-        let auth = new AuthController();
-        const canUseRoute = auth.checkJwt(jwt);
-        if (canUseRoute) return true;
-        else return false;
-    }
+  async all(request: Request, response: Response, next: NextFunction): Promise<Array<Location>> {
+    return this.TokenVerifier.tokenMiddleware(response, this.TokenVerifier.verifyToken(request), await this.LocationRepository.find())
+  }
 
-    private tokenMiddleware(response: Response, hasToken: Boolean, dbResponse) {
-        return hasToken ? dbResponse : response.status(401).send("Authorization falied")
+  async one(request: Request, response: Response, next: NextFunction): Promise<Location | undefined> {
+    try {
+      return this.TokenVerifier.tokenMiddleware(response, this.TokenVerifier.verifyToken(request), this.LocationRepository.find({ where: { idLocation: request.params.id }, take: 1 }))
     }
+    catch{
+      return response.status(404)
+    }
+  }
 
-    async all(request: Request, response: Response, next: NextFunction): Promise<Array<Location>> {
-        return this.tokenMiddleware(response, this.verifyToken(request), await this.LocationRepository.find())
-    }
+  async create(request: Request, response: Response, next: NextFunction): Promise<Location | undefined> {
+    return this.TokenVerifier.tokenMiddleware(response, this.TokenVerifier.verifyToken(request), await this.LocationRepository.save(request.body))
+  }
 
-    async one(request: Request, response: Response, next: NextFunction): Promise<Location | undefined> {
-        try {
-            return this.tokenMiddleware(response, this.verifyToken(request), this.LocationRepository.find({ where: { idLocation: request.params.id }, take: 1 }))
-        }
-        catch{
-            return response.status(404)
-        }
-    }
-
-    async create(request: Request, response: Response, next: NextFunction): Promise<Location | undefined> {
-        console.log(request)
-        return this.tokenMiddleware(response, this.verifyToken(request), await this.LocationRepository.save(request.body))
-    }
-
-    async remove(request: Request, response: Response, next: NextFunction): Promise<void> {
-        const LocationToRemove = await this.LocationRepository.findOne(request.params.id)
-        await this.LocationRepository.remove(LocationToRemove)
-    }
+  async remove(request: Request, response: Response, next: NextFunction): Promise<void> {
+    const LocationToRemove = await this.LocationRepository.findOne(request.params.id)
+    await this.LocationRepository.remove(LocationToRemove)
+  }
 }
